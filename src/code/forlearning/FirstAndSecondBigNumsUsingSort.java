f35afe7da4fe7ed45d1570c53644cf010d08dfae
Venkata-Adapala <92817078+Venkata-Adapala@users.noreply.github.com>
2022-02-03 12:48:36 -0500

    Updated in my branch

--- src/code/forlearning/FirstAndSecondBigNumsUsingSort.java
+++ src/code/forlearning/FirstAndSecondBigNumsUsingSort.java
@@ -7,10 +7,16 @@ public class FirstAndSecondBigNumsUsingSort {
    public static void main(String [] args) {
 
        int [] arrNums= {45,89,23,194,71,91,26};
+       
+       System.out.println("Before sorting the array numbers");
+       for (int i: arrNums)
+    	   System.out.println(i);
 
        //use the built in sort method available in Arrays class
        Arrays.sort(arrNums);
-
+       System.out.println("After sorting the array numbers");
+       for (int i: arrNums)
+    	   System.out.println(i);
        System.out.println("The biggest number in the array is="+arrNums[arrNums.length-1]);
        System.out.println("The second biggest number in the array is="+arrNums[arrNums.length-2]);
    }
